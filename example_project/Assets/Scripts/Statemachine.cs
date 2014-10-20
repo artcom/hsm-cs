89c396c5e3b403643fe90b5d8545e08ca29d64ae
Andreas Marr <andreasm@artcom.de>
2014-10-20 16:50:49 +0200

    Added example project (which currently also contains the tests!)

---
+++ example_project/Assets/Scripts/Statemachine.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+using Hsm;
+
+public class Statemachine : MonoBehaviour {
+
+	public Text debugText;
+	[SerializeField]
+	public StateMachine sm;
+
+	void Start() {
+		sm = new StateMachine();
+
+		// Idle State
+		State idleState = new State("idle")
+		.OnEnter((sourceState, targetState) => {
+			Debug.Log(targetState.id + " -> on_enter (from: " +
+			((sourceState != null) ? sourceState.id : "null") + ")");
+		})
+		.OnExit((nextState) => {
+			Debug.Log("idle exit -> " + nextState.id);
+		}).addHandler("start", (data) => {
+    	    if (data.ContainsKey("powered") && data["powered"].Equals(true)) {
+			    return "off";
+			}
+		    return null;
+		});
+
+		// On State
+		State onState = new State("on")
+		.OnEnter((sourceState, targetState) => {
+			Debug.Log(targetState.id + " -> on_enter (from: " +
+			((sourceState != null) ? sourceState.id : "null") + ")");
+		})
+		.OnExit((nextState) => {
+			Debug.Log("on exit -> " + nextState.id);
+		}).addHandler("off", (data) => {
+			return "off";
+		});
+
+		// Off State
+		State offState = new State("off")
+		.OnEnter((sourceState, targetState) => {
+			Debug.Log(targetState.id + " -> on_enter (from: " +
+			((sourceState != null) ? sourceState.id : "null") + ")");
+		})
+		.OnExit((nextState) => {
+			Debug.Log("on on_exit -> " + nextState.id);
+		}).addHandler("on", (data) => {
+			return "on";
+		});
+		
+		sm.addState(idleState)
+		.addState(onState)
+		.addState(offState);
+		debugText.text = "uninitialized";
+
+		try {
+			sm.setup();
+		} catch(UnityException e) {
+			Debug.LogException(e);
+			debugText.text = e.Message;
+		}
+
+		/*sm.handleEvent("start", new Dictionary<string, object>{
+			{"powered", true}
+		});*/
+		sm.handleEvent("start");
+		sm.handleEvent("on");
+		sm.handleEvent("off");
+	}
+
+	void Awake() {
+	}
+}
+
+
+[CustomPropertyDrawer(typeof(Statemachine))]
+public class StateMachineDrawer : PropertyDrawer {
+
+	bool unfolded = false;
+	int numStates = 0;
+
+	public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
+	{
+		if (unfolded) {
+			return base.GetPropertyHeight (property, label) + 20 * numStates + 10;
+		} else {
+			return base.GetPropertyHeight (property, label);
+		}
+	}
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		// SerializedProperty currentStateProp = property.FindPropertyRelative("currentState");
+		// SerializedProperty initialStateProp = property.FindPropertyRelative("initialState");
+		SerializedProperty statesProp = property.FindPropertyRelative("states");
+		numStates = statesProp.arraySize;
+
+		EditorGUI.BeginProperty(position, label, property);
+		unfolded = EditorGUI.Foldout(position, unfolded, label);
+		EditorGUI.EndProperty();
+
+		position.y += 20;
+		position.height = 20;
+
+		if (unfolded) {
+			for (int i = 0; i < statesProp.arraySize; i++) {
+				EditorGUI.PropertyField(position, statesProp.GetArrayElementAtIndex(i).FindPropertyRelative("id"));
+				position.y += 20;
+			}
+		}
+	}
+}
---
+++ example_project/Assets/lib/hsm/State.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace Hsm {
+
+	[System.Serializable]
+	public class State {
+		[SerializeField]
+		public string id;
+		
+		private Action<State, State> enterAction = null;
+		private Action<State> exitAction = null;
+		
+		public Dictionary<string, Func<Dictionary<string, object>, String>> handlers; // = new Dictionary<string, Action>();
+		
+		public State(string pId) {
+			this.handlers = new Dictionary<string, Func<Dictionary<string, object>, String>>();
+			id = pId;
+		}
+		
+		public State addHandler(string evt, Func<Dictionary<string, object>, String> handler) {
+			handlers[evt] = handler;
+			return this;
+		}
+		
+		public void _enter(State sourceState, State targetstate, Dictionary<string, object> data) {
+			if (enterAction != null) {
+				enterAction.Invoke(sourceState, targetstate);
+			}
+		}
+		
+		public void _exit(State nextstate) {
+			if (exitAction != null) {
+				exitAction.Invoke(nextstate);
+			}
+		}
+		
+		public State OnEnter(Action<State, State> action) {
+			enterAction = action;
+			return this;
+		}
+		
+		public State OnExit(Action<State> action) {
+			exitAction = action;
+			return this;
+		}
+	}
+
+}
